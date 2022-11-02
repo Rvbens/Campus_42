b73342585db5218a88e46e17e9201de354563153
Ruben Chaves Moreno <rchaves-@c1r12s2.42malaga.com>
2022-11-02 13:39:16 +0100

    Fixes

--- ex01/ft_strncmp.c
+++ ex01/ft_strncmp.c
@@ -15,6 +15,8 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 	unsigned int	i;
 
 	i = 0;
+	if (n == 0)
+		return (0);
 	while (*s1 && *s2 && i < n - 1)
 	{
 		if (*s1 != *s2)
--- ex05/ft_strlcat.c
+++ ex05/ft_strlcat.c
@@ -12,24 +12,26 @@
 
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
+	unsigned int	dst_l;
+	unsigned int	src_l;
 	unsigned int	i;
-	unsigned int	j;
 
+	dst_l = 0;
+	while (dest[dst_l])
+		dst_l++;
+	src_l = 0;
+	while (src[src_l])
+		src_l++;
+	if (size == 0 || size < dst_l + 1)
+		return (src_l + size);
 	i = 0;
-	while (dest[i])
+	while (src[i] && i + dst_l + 1 < size)
+	{
+		dest[dst_l + i] = src[i];
 		i++;
-	j = 0;
-	while (*src && j < size - i - 1)
-	{
-		dest[i + j] = *src;
-		src++;
-		j++;
 	}
-	dest[i + j] = 0;
-	while (*src)
-	{
-		src++;
-		j++;
-	}
-	return (i + j);
+	dest[dst_l + i] = 0;
+	while (src[i])
+		i++;
+	return (dst_l + i);
 }
